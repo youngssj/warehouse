57419a08e604145068aadf80cbea4e223fddf397
victor <wangchunxiaocr@126.com>
2025-03-03 09:45:07 +0800

    入库修改

--- module-workbench/src/main/java/com/victor/workbench/ui/viewmodel/ScanLocationViewModel.java
+++ module-workbench/src/main/java/com/victor/workbench/ui/viewmodel/ScanLocationViewModel.java
@@ -3,22 +3,32 @@ package com.victor.workbench.ui.viewmodel;
 import android.app.Application;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.ObservableField;
 
 import com.victor.base.base.BaseTitleViewModel;
 import com.victor.base.data.Repository.AppRepository;
 import com.victor.base.data.entity.LocationBean;
 import com.victor.base.data.http.ApiDisposableObserver;
 
+import me.goldze.mvvmhabit.binding.command.BindingCommand;
 import me.goldze.mvvmhabit.bus.event.SingleLiveEvent;
 import me.goldze.mvvmhabit.utils.RxUtils;
 
 public class ScanLocationViewModel extends BaseTitleViewModel<AppRepository> {
+
+    public ObservableField<LocationBean> location = new ObservableField<>();
 
     public class UIChangeObservable {
         public SingleLiveEvent<LocationBean> scanLocationEvent = new SingleLiveEvent<>();
     }
 
     public UIChangeObservable uc = new UIChangeObservable();
+
+    public ObservableField<Boolean> btnVisiable = new ObservableField<>(false);
+
+    public BindingCommand confirmClickCommand = new BindingCommand(() -> {
+        uc.scanLocationEvent.setValue(location.get());
+    });
 
     public ScanLocationViewModel(@NonNull Application application, AppRepository model) {
         super(application, model);
@@ -31,7 +41,10 @@ public class ScanLocationViewModel extends BaseTitleViewModel<AppRepository> {
                 .subscribe(new ApiDisposableObserver<LocationBean>() {
                     @Override
                     public void onResult(LocationBean locationBean) {
-                        uc.scanLocationEvent.setValue(locationBean);
+                        if (locationBean != null) {
+                            btnVisiable.set(true);
+                            location.set(locationBean);
+                        }
                     }
 
                     @Override
